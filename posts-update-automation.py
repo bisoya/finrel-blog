79511ea7beeb533c23a1e5424aff7747ba704bbb
jectgenius <jkl9087@naver.com>
2024-11-11 11:44:59 +0900

    Update:(/posts-update-automation.py): 카테고리와 최근 발행 포스트 위아래 순서 수정 #2 #2

--- posts-update-automation.py
+++ posts-update-automation.py
@@ -68,7 +68,7 @@ def update_readme(category: str):
     if readme.find(category) == -1:
         with open("README.md", "a", encoding="utf-8") as f:
             # f.write(f"\n- [{category}]({GITHUB_URI + category}){:target="_blank"}")
-            f.write(f'\n- <a href="{GITHUB_URI + category}" target="_blank">{category}</a>\n')
+            f.write(f'\n\n- <a href="{GITHUB_URI + category}" target="_blank">{category}</a>\n')
 
     sort_toc()
 
